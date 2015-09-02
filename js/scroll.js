665f6aa8299950ff56daeba11aac7532d123fb39
MykhailoBondarev <toki731@gmail.com>
2015-09-02 17:47:08 +0300

    dfewfw

--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,7 @@ var target = $(this).attr('href');
 $('html, body').animate({scrollTop: $(target).offset().top}, 2000);
 return false;
 });
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
